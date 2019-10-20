947f7c2bb9598939399aa1975ef3c5de7965c85a
Chandler Watson <watsonc@stanford.edu>
2019-10-19 18:30:18 -0700

    Fix bugs in recap.py

--- recap.py
+++ recap.py
@@ -11,21 +11,26 @@ from tf_pose.networks import get_graph_path, model_wh
 # This work is based on the run_camera.py script incluced in
 # https://github.com/ildoonet/tf-pose-estimation.
 
+# To run the below script, first install tf-pose-estimation
+# via `pip install tf-pose-estimation`.
+
 fps_time = 0
 
 CAMERA = 0
 WIDTH, HEIGHT = 368, 368
 MODEL = 'mobilenet_thin'
 ESC_KEY = 27
+UPSAMPLE_SIZE = 4.0
 
 estimator = TfPoseEstimator(get_graph_path(MODEL), target_size=(WIDTH, HEIGHT))
-cam = cv2.VideoCapture(args.camera)
-_, image = cam.read()
+cam = cv2.VideoCapture(CAMERA)
 
 while True:
     _, image = cam.read()
-    humans = e.inference(image, resize_to_default=True)
+    humans = estimator.inference(image, resize_to_default=True,
+                                 upsample_size=UPSAMPLE_SIZE)
     image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+    cv2.imshow('Recap', image)
     if cv2.waitKey(1) == ESC_KEY:
         break
 
